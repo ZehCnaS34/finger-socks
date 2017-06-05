5fe0996b819578581aabfa9edd18e8d1fad1f0d6
Alexander Sanchez <asanchez.dev@gmail.com>
2017-06-05 15:39:14 -0400

    Alright, I need to get back to my other work

--- src/App.js
+++ src/App.js
@@ -2,11 +2,14 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
 
-const SentenceLineDisplay = ({targetText, inputText}) => {
+const SentenceLineDisplay = ({targetText, inputText, onError}) => {
   let spans = inputText.split('').map((char, idx) => {
     if (char === targetText[idx]) {
       return <span key={idx} style={{color: 'green'}}>{char}</span>
     }
+
+    // NOTE: causes infinite render loop
+    // onError && setTimeout(() => onError(char), 0);
 
     if (char === ' ') {
       return (targetText[idx]) ? (
@@ -51,10 +54,17 @@ class App extends Component {
   state = {
     inputText: "",
     lineFeed: [
-      "Roses are red.",
+      "an out-of-date bookmark or favorite",
+      "a search engine that has an out-of-date listing for this page",
+      "a mistyped address",
+      "a page access restriction",
+      "the requested resource was not found",
+      "an error has occurred while processing your request."
     ],
     completedLines: [
-    ]
+    ],
+    times: [],
+    errors: {}
   }
 
   /** 
@@ -70,6 +80,18 @@ class App extends Component {
     }
   }
 
+  registerError = (char) => {
+    console.log(char)
+    if (this.state.errors[this.state.completedLines.length]) {
+    } else {
+      this.state.errors[this.state.completedLines] = 0
+    }
+    this.state.errors[this.state.completedLines.length]++
+    this.setState({
+      errors: this.state.errors
+    })
+  }
+
   restartRacer = () => {
     this.setState({
       lineFeed: this.state.completedLines,
@@ -83,9 +105,11 @@ class App extends Component {
         { this.state.lineFeed.length > 0 ? (
           <div>
             <SentenceLineDisplay
+              onError={this.registerError}
               inputText={this.state.inputText}
               targetText={this.state.lineFeed[0]} />
             <PressEnter a={this.state.inputText} b={this.state.lineFeed[0]} />
+            <h3>Errors: {this.state.errors[this.state.completedLines.length] || 0}</h3>
             <input 
               autoFocus={true}
               type="text"
