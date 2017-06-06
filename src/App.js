9000d2251d6cd076f8e0923d834279c6ea13d206
Alexander Sanchez <asanchez.dev@gmail.com>
2017-06-06 15:29:39 -0400

    Switching to word based machting

--- src/App.js
+++ src/App.js
@@ -1,149 +1,93 @@
 import React, { Component } from 'react';
 // import logo from './logo.svg';
 import './App.css';
+import { zipMap } from './util';
 
-
-function zipMap(lst1, lst2, fn) {
-  let output = [];
-  let [bigger, smaller] = lst1.length >= lst2.length ? 
-    [lst1, lst2]: 
-    [lst2, lst1];
-  let [bi, si] = lst1.length >= lst2.length ? 
-    [0, 1]: 
-    [1, 0];
-
-  for (let x = 0; x < bigger.length; x++) {
-    let payload = [undefined, undefined];
-    if (x >= smaller.length) {
-      payload[bi] = bigger[x];
-      payload[si] = null;
-      output.push(fn(payload, x));
-    } else {
-      payload[bi] = bigger[x];
-      payload[si] = smaller[x];
-      output.push(fn(payload, x));
-    }
-  }
-  return output;
-}
-
-
-const SentenceLineDisplay = ({targetText, inputText, onError}) => (
-  <pre>
-    {
-      zipMap(inputText.split(''), targetText.split(''), ([inputChar, targetChar], idx) => {
-        // when we have a match
-        if (inputChar === targetChar) {
-          return <span key={idx} style={{color: 'green'}}>{targetChar}</span>
-        } 
-
-        // when you've done something wrong
-        if (idx < inputText.length) {
-          if (targetChar && targetChar != ' ') {
-            return <span key={idx} style={{textDecoration: 'underline', color: 'red'}}>{targetChar}</span>
-          }
-          return <span key={idx} style={{backgroundColor: 'red'}}>{' '}</span>
-        }
-
-        // lets draw the cursor
-        if (idx == inputText.length) {
-          return <span key={idx} style={{textDecoration: 'underline'}}>{targetChar}</span>
-        }
-
-        // relax we haven't gotten there yet.
-        return <span key={idx} style={{color: 'black'}}>{targetChar}</span>
-      })
-    }
-  </pre>
-);
-
-
-const PressEnter = ({a, b}) => (a.length === b.length) ? (
-  <p>Press Enter</p>
-) : (a.length >= b.length) ? (
-  <p>you went overboard</p>
-) : (
-  <p>keep on going!</p>
-);
-
+import { WordMatcher } from './WordMatcher';
 
 class App extends Component {
   state = {
-    inputText: "",
-    lineFeed: "Search for Simple poems, articles about Simple poems, poetry blogs, or anything else Simple poem related using the PoetrySoup search engine at the top of the page.".split(",").map(s => s.trim()),
-    completedLines: [
-    ],
-    times: [],
-    errors: {}
+    wordFeed: [],
+    completedWords: [],
+
+    target: '',
+    value: ''
   }
 
-  percentComplete = () => {
-    return (this.state.completedLines.length/(this.state.lineFeed.length+this.state.completedLines.length))*100
-  }
+  componentDidMount() {
+    let wordFeed = "This is a test".split(' '),
+      target = wordFeed.shift()
 
-  /** 
-   * This function move
-   */
-  newLine = (e) => {
-    let keyCode = e.keyCode || e.which
-    if (keyCode === 13) {
-      if (this.state.inputText === this.state.lineFeed[0]) {
-        this.state.completedLines.push(this.state.lineFeed.shift())
-        this.setState({lineFeed: this.state.lineFeed, inputText: ""})
-      }
-    }
-  }
-
-  registerError = (char) => {
-    // console.log(char)
-    if (this.state.errors[this.state.completedLines.length]) {
-    } else {
-      this.state.errors[this.state.completedLines] = 0
-    }
-    this.state.errors[this.state.completedLines.length]++
     this.setState({
-      errors: this.state.errors
+      wordFeed,
+      target
     })
   }
 
-  restartRacer = () => {
+  checkForWordMatch = (e) => {
+    if (this.state.value === this.state.target) {
+      let completed = this.state.completedWords;
+      let wordFeed = this.state.wordFeed;
+      completed.push(this.state.target);
+      let target = wordFeed.shift();
+      e.preventDefault()
+      this.setState({
+        wordFeed,
+        completedWords: completed,
+        target,
+        value: ''
+      })
+
+    }
+  }
+
+  handleChar = (e) => {
     this.setState({
-      lineFeed: this.state.completedLines,
-      completedLines: []
+      value: this.state.value + String.fromCharCode(e.charCode)
     })
+  }
+
+  handleKey = (e) => {
+    if (e.keyCode === 8) {
+      this.setState({
+        value: this.state.value.substr(
+          0,
+          this.state.value.length -1
+        )
+      })
+    }
+    if (e.keyCode === 32) {
+      this.checkForWordMatch(e);
+      console.log(e.keyCode, this.state.value)
+    }
   }
 
   render() {
     return (
       <div className="App">
-        { this.state.lineFeed.length > 0 ? (
-          <div>
-            {this.state.completedLines.map(line => <pre style={{color: 'green'}}>{line}</pre>)}
-            <SentenceLineDisplay
-              onError={this.registerError}
-              inputText={this.state.inputText}
-              targetText={this.state.lineFeed[0]} />
-            {this.state.lineFeed.filter((v,i) => i != 0).map(line => <pre style={{color: 'grey'}}>{line}</pre>)}
-            <PressEnter a={this.state.inputText} b={this.state.lineFeed[0]} />
-            <h3>Errors: {this.state.errors[this.state.completedLines.length] || 0}</h3>
-            <h3>Percent Done: {this.percentComplete()}%</h3>
-            <input 
-              autoFocus={true}
-              type="text"
-              value={this.state.inputText} 
-              onKeyPress={this.newLine}
-              onChange={(e) => {
-                return this.setState({inputText: e.target.value})}
-              } />
-          </div>
-        ) : (
-          <div>
-            <h1>DONE</h1>
-            <h3>Percent Done: {this.percentComplete()}%</h3>
-            <button onClick={this.restartRacer}>Restart</button>
-          </div>
-        )
-        }
+        <p>
+          {
+            (this.state.target) ? (
+              <div>
+              {this.state.completedWords.map((word, idx) => (
+                <span style={{color: 'green', marginLeft: '5px', marginRight: '5px'}}key={idx}>{word}</span>
+
+              ))}
+              <WordMatcher target={this.state.target} current={this.state.value} />
+              {this.state.wordFeed.map((word, idx) => (
+                <span style={{marginLeft: '5px', marginRight: '5px'}}key={idx}>{word}</span>
+              ))}
+            </div>
+            ) : (
+              <h1>wait</h1>
+            )
+          }
+        </p>
+        <input
+          autoFocus
+          value={this.state.value}
+          onKeyDown={this.handleKey}
+          onKeyPress={this.handleChar} />
       </div>
     );
   }
---
+++ src/WordMatcher.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { zipMap } from './util'
+
+console.log(zipMap);
+
+export const WordMatcher = ({current, target}) => {
+  if (current.length > target.length) {
+    return <span style={{backgroundColor: 'red'}}>
+      {current}
+    </span>
+  }
+  let o = zipMap(current.split(''), target.split(''), ([c, t], idx) => {
+    if (c == t) {
+      return <span key={idx} style={{backgroundColor: 'green', textDecoration: 'underline'}}>{t}</span>
+    }
+
+    if (t === undefined) {
+      return <span key={idx} style={{backgroundColor: 'red'}}>{' '}</span>
+    }
+
+    if (idx < current.length) {
+      return <span key={idx} style={{backgroundColor: 'red'}}>{t}</span>
+    }
+
+    return  <span key={idx} style={{backgroundColor: 'grey', textDecoration: 'underline'}}>{t}</span>
+  })
+  if (o.length > 0) {
+    return <span style={{marginRight: '5px', marginLeft: '5px'}}>{o}</span>;
+  }
+  return null
+}
